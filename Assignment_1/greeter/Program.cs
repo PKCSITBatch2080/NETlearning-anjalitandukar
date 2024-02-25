373a64611b986996d3b748fa10547c53133243e6
anjali5s <tandukaranjali5@gmail.com>
2024-02-25 21:01:37 +0545

    Assignment_1

---
+++ Assignment_1/greeter/Program.cs
@@ -0,0 +1,49 @@
+﻿// using System;
+// class Program
+// {
+//     static void Main()
+//     {
+//         string fullName ;
+//         Console.ReadLine();
+
+//         Console.WriteLine("Please enter your date of birth (MM/DD/YYYY):");
+//         string dobString = Console.ReadLine();
+//         DateTime dob;
+//         if (DateTime.TryParse(dobString, out dob))
+//         {
+            
+//             string birthDateString = dob.ToString("MM/dd/yyyy");
+//             Console.WriteLine($"Your date of birth is: {birthDateString}");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Invalid date format entered.");
+//         }
+//     }
+// }
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fullName ;
+       Console.ReadLine();
+
+        Console.WriteLine("Please enter your date of birth (YYYY-MM-DD):");
+        DateTime dob;
+        while (!DateTime.TryParse(Console.ReadLine(), out dob))
+        {
+            Console.WriteLine("Invalid date format. Please enter your date of birth in YYYY-MM-DD format:");
+        }
+        DateTime currentDate = DateTime.Now;
+        int ageYears = currentDate.Year - dob.Year;
+        int ageMonths = currentDate.Month - dob.Month;
+        int ageDays = currentDate.Day - dob.Day;
+        if (ageMonths < 0 || (ageMonths == 0 && ageDays < 0))
+        {
+            ageYears--;
+            ageMonths += 12;
+        }
+        Console.WriteLine($"Age as of now: {ageYears} years, {ageMonths} months, and {ageDays} days.");
+    }
+}
---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// integer number
+byte a=255 ;
+byte dayOfWeek=11; //camel casimg
+byte DayOfWeek=1; //pascal casing 
+short b=23333;
+int c=23322;
+long d=2242525;
+
+// float num
+float weight = 45.633f; // yo float num ho vanera chinauxa last ko f le 
+double weight2 = 45678.22222;
+decimal weight3=22525.5252522525m; //sim m le 
+
+//text
+char initial = 'D';
+string name="Anjali Tandukar";
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(name);
+Console.WriteLine(initial);
+
+Console.WriteLine(weight);
+Console.WriteLine(weight2);
+Console.WriteLine(weight3);
+Console.WriteLine(dayOfWeek);
+Console.WriteLine(DayOfWeek);
